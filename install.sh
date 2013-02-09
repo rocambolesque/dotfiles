a38978395c4c18aa804c5586d6722f29ae8593c5
rocdose <rocdose@gmail.com>
2013-02-09 17:39:31 +0100

    Adds bundle required packages install

--- install.sh
+++ install.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 ROOT="`pwd`"
+VIM_PACKAGES=(ack-grep exuberant-ctags)
 
 set -e
 
 info () {
-    printf "  [ \033[00;34mINFO\033[0m ] $1\n"
+    printf "\r\033[00;m$1\033[0m\n"
+}
+
+warning () {
+    printf "\r\033[00;33m$1\033[0m\n"
+}
+
+error () {
+    printf "\r\033[00;31m$1\033[0m\n"
 }
 
 success () {
-    printf "\r\033[2K  [ \033[00;32mSUCCESS\033[0m ] $1\n"
+    printf "\r\033[00;32m$1\033[0m\n"
 }
 
 install () {
@@ -29,13 +38,39 @@ install () {
 }
 
 setup_vim () {
+    info "setting up vim"
+    vim_version=`vim --version | head -1 | grep -o '[[:digit:]]\.[[:digit:]]'` 
+    info 'vim version: '$vim_version
+}
+
+install_vim_bundles () {
     info 'installing vim bundles'
-
-    vim -c 'BundleInstall' -c 'qa'
-
+    #vim -c 'BundleInstall' -c 'qa'
     success 'bundles installed'
+}
+
+install_vim_bundles_requirements () {
+    info "installing vim bundles requirements"
+    to_install=""
+    for package in "${VIM_PACKAGES[@]}"
+    do
+        status=`aptitude search "${package}" | head -1 | grep -o '^.'`
+        if [ "$status" != "i" ]
+        then
+            warning 'vim required package: '"${package}"' not installed'
+            to_install+=" "$package
+        else
+            info 'vim required package: '"${package}"' installed'
+        fi
+    done
+    if [ "$to_install" != "" ]
+    then
+        warning "run apt-get install$to_install"
+    fi
 }
 
 install
 setup_vim
+install_vim_bundles
+install_vim_bundles_requirements
 info "done"
